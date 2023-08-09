d1af2074738a567cf6a699a7fd4f065e67ef819b
Daniel Passy <danielpassy@hotmail.com>
2023-08-09 16:56:37 -0300

    cors allowed

--- backend/desafio_moavi/settings.py
+++ backend/desafio_moavi/settings.py
@@ -44,9 +44,11 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "core",
     "django_extensions",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
